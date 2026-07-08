2602e3a31ac260aaa87f3214ce259595990d3cac
Dorian Grasset <doriangrasset@proton.me>
2026-07-08 22:35:24 +0200

    Initial Commit

---
+++ mute/muteApp.swift
@@ -0,0 +1,17 @@
+//
+//  muteApp.swift
+//  mute
+//
+//  Created by Dorian Grasset on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct muteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
